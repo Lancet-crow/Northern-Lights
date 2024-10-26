cc90478d60eef3e06fcf64aba0ba544f42b1da8c
Alex-Degtyarenko <aleksdegtyarenko20@gmail.com>
2024-10-26 14:02:24 +0400

    Initial commit

---
+++ src/main/java/lancet_/northern_lights/NorthernLightsDataGenerator.java
@@ -0,0 +1,11 @@
+package lancet_.northern_lights;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NorthernLightsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
