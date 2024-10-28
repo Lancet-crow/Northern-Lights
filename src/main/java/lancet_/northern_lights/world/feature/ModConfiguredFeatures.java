c6a424d1f300bd64fb7d88a490722a65cb984861
Alex-Degtyarenko <aleksdegtyarenko20@gmail.com>
2024-10-28 17:47:20 +0400

    Fixed: Heather generation! Now heather spawns in Meadow biome, and it is pretty rare(as it was designed). I don't really like its texture, though...

--- src/main/java/lancet_/northern_lights/world/feature/ModConfiguredFeatures.java
+++ src/main/java/lancet_/northern_lights/world/feature/ModConfiguredFeatures.java
@@ -1,45 +1,16 @@
 package lancet_.northern_lights.world.feature;
 
 import lancet_.northern_lights.NorthernLights;
-import lancet_.northern_lights.block.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public class ModConfiguredFeatures {
+    public static final RegistryKey<ConfiguredFeature<?, ?>> HEATHER_CONFIGURED_KEY = registerConfiguredFeatureKey("heather");
 
 
-    //public static final RegistryKey<ConfiguredFeature<?, ?>> HEATHER_PATCH_KEY = registerFeatureKey("heather_patch");
-
-    /*public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context){
-        NorthernLights.LOGGER.info("Adding Heather Patch to Configured Features");
-        register(context, HEATHER_PATCH_KEY, Feature.RANDOM_PATCH,
-                ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK,
-                        new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.HEATHER)))
-        );
-        register(context, HEATHER_KEY, Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(
-                BlockStateProvider.of(ModBlocks.HEATHER)));
-        register(context, HEATHER_PATCH_KEY, Feature.FLOWER,
-                new RandomPatchFeatureConfig(
-                        64, 6, 2, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
-                        new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.HEATHER)))
-                )
-        );
+    private static RegistryKey<ConfiguredFeature<?,?>> registerConfiguredFeatureKey(String name) {
+        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, Identifier.of(NorthernLights.MOD_ID, name));
     }
-
-    private static RandomPatchFeatureConfig createRandomPatchFeatureConfig(Block block, int tries) {
-        return ConfiguredFeatures.createRandomPatchFeatureConfig(tries, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(block))));
-    }*/
-
-
-
-    /*private static <FC extends FeatureConfig, F extends Feature<FC>> void register(
-            Registerable<ConfiguredFeature<?, ?>> context, RegistryKey<ConfiguredFeature<?, ?>> key,
-            F feature, FC featureConfig){
-        context.register(key, new ConfiguredFeature<>(feature, featureConfig));
-    }*/
 }
--- src/main/java/lancet_/northern_lights/world/feature/ModPlacedFeatures.java
+++ src/main/java/lancet_/northern_lights/world/feature/ModPlacedFeatures.java
@@ -1,45 +1,15 @@
 package lancet_.northern_lights.world.feature;
 
 import lancet_.northern_lights.NorthernLights;
-import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.gen.blockpredicate.BlockPredicate;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import net.minecraft.world.gen.feature.PlacedFeature;
-import net.minecraft.world.gen.feature.PlacedFeatures;
-import net.minecraft.world.gen.placementmodifier.*;
-
-import java.util.List;
 
 public class ModPlacedFeatures {
+    public static final RegistryKey<PlacedFeature> HEATHER_PLACED_KEY = registerPlacedFeatureKey("heather_placed");
 
-    //public static final RegistryKey<PlacedFeature> HEATHER_PATCH_KEY = registerFeatureKey("heather_patch_placed");
-
-
-    /*public static void bootstrap(Registerable<PlacedFeature> context){
-        RegistryEntryLookup<ConfiguredFeature<?, ?>> registryLookup = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
-        NorthernLights.LOGGER.info("Adding Heather Patch to Placed Features");
-        register(context, HEATHER_KEY, registryLookup.getOrThrow(ModConfiguredFeatures.HEATHER_KEY),
-                List.of(BlockFilterPlacementModifier.of(BlockPredicate.IS_AIR)));
-        register(context, HEATHER_PATCH_KEY, registryLookup.getOrThrow(ModConfiguredFeatures.HEATHER_PATCH_KEY),
-                Modifiers.modifiers(12));
-    }*/
-
-
-
-    /*private static void register(Registerable<PlacedFeature> context, RegistryKey<PlacedFeature> key,
-                                 RegistryEntry<ConfiguredFeature<?, ?>> config,
-                                 List<PlacementModifier> modifierList){
-        context.register(key, new PlacedFeature(config, List.copyOf(modifierList)));
+    private static RegistryKey<PlacedFeature> registerPlacedFeatureKey(String name){
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(NorthernLights.MOD_ID, name));
     }
-    public static class Modifiers{
-        public static List<PlacementModifier> modifiers(int count) {
-            return List.of(CountPlacementModifier.of(count), SquarePlacementModifier.of(), PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP, BiomePlacementModifier.of());
-        }
-    }*/
 }
